907465508950558ab3a8a64f4e13202f78ae61fa
code <43371021+zqlpaopao@users.noreply.github.com>
2022-03-14 12:54:56 +0800

    Update main.go

--- redis/main.go
+++ redis/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main(){
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/zqlpaopao/tool/redis/pkg"
+)
 
+func main() {
+	//lua := pkg.MakeNewScript(pkg.GetStrSetStr)
+
+	redis := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+	})
+
+	res := redis.Eval(context.TODO(), pkg.GetStrSetStr, []string{"test1"}, []interface{}{"test1",600})
+	fmt.Println(res.StringSlice())
 }
