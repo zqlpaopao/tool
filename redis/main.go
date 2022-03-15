1f914b1defb5dac859cf1d2afbfe398afa88f4c9
code <43371021+zqlpaopao@users.noreply.github.com>
2022-03-15 18:51:37 +0800

    Update main.go

--- redis/main.go
+++ redis/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
-	"github.com/zqlpaopao/tool/redis/pkg"
 )
 
 func main() {
@@ -14,6 +13,14 @@ func main() {
 		Addr: "127.0.0.1:6379",
 	})
 
-	res := redis.Eval(context.TODO(), pkg.GetStrSetStr, []string{"test1"}, []interface{}{"test1",600})
+	//str := redis.ScriptLoad(context.TODO(),pkg.IncrNum)
+	str := "53b0036f258e3fe5393a8aed723a828b10483685"
+	fmt.Println(str)
+
+	res := redis.EvalSha(context.TODO(), str, []string{"testa"}, []interface{}{18,4,400})
+
+
+	//res := redis.Eval(context.TODO(), pkg.IncrNum, []string{"testa"}, []interface{}{13,4,400})
 	fmt.Println(res.StringSlice())
+
 }
