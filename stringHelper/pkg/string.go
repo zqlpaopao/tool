2552f7c1112f1aa451d47471dbedd062a6035453
code <43371021+zqlpaopao@users.noreply.github.com>
2022-07-11 17:22:11 +0800

    Update string.go

--- stringHelper/pkg/string.go
+++ stringHelper/pkg/string.go
@@ -23,6 +23,16 @@ func StringFromAssertionFloat(number interface{}) string {
 		numberString = strconv.FormatInt(int64(floatOriginal), 10)
 	case int64:
 		numberString = strconv.FormatInt(floatOriginal, 10)
+	case uint:
+		numberString = strconv.FormatInt(int64(floatOriginal), 10)
+	case uint8:
+		numberString = strconv.FormatInt(int64(floatOriginal), 10)
+	case uint32:
+		numberString = strconv.FormatInt(int64(floatOriginal), 10)
+	case uint16:
+		numberString = strconv.FormatInt(int64(floatOriginal), 10)
+	case uint64:
+		numberString = strconv.FormatInt(int64(floatOriginal), 10)
 	case []uint8:
 		numberString = string(floatOriginal)
 		break
@@ -33,25 +43,25 @@ func StringFromAssertionFloat(number interface{}) string {
 }
 
 //StringFromAssertionFloatNotZero 判断结构体的类型是不是指针类型
-func StringFromAssertionFloatNotZero(ty string,vl reflect.Value) (b bool,numberString string) {
+func StringFromAssertionFloatNotZero(ty string, vl reflect.Value) (b bool, numberString string) {
 
 	switch ty {
-	case "float","float8","float16","float32","float64":
+	case "float", "float8", "float16", "float32", "float64":
 		//numberString = strconv.FormatInt(int64(vl.Float()), 10)
-		if vl.Interface() == 0{
+		if vl.Interface() == 0 {
 			return
 		}
-	case "int","int8","int16","int32","int64":
-		if vl.Interface() == 0{
+	case "int", "int8", "int16", "int32", "int64":
+		if vl.Interface() == 0 {
 			return
 		}
 		numberString = strconv.FormatInt(int64(vl.Int()), 10)
 	case "string":
-		if vl.String() == ""{
+		if vl.String() == "" {
 			return
 		}
 		b = true
-	case "*string","*int","*int8","*int16","*int32","*int64","*float","*float8","*float16","*float32","*float64":
+	case "*string", "*int", "*int8", "*int16", "*int32", "*int64", "*float", "*float8", "*float16", "*float32", "*float64":
 		//numberString = strconv.FormatInt(int64(vl.Float()), 10)
 		//if vl.Elem().String()
 		b = true
