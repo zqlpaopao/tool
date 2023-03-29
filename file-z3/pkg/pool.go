2dfc62f0aff9a9ba48fe51e754c91aa7d3385f94
code <43371021+zqlpaopao@users.noreply.github.com>
2023-03-29 10:20:06 +0800

    Update pool.go

--- file-z3/pkg/pool.go
+++ file-z3/pkg/pool.go
@@ -1,25 +1,25 @@
 package pkg
 
 // GetByteSliceBuf reusable objects
-func (o *Option) GetByteSliceBuf() (res []byte) {
+func (o *Option) GetByteSliceBuf() []byte {
 	select {
-	case res = <-o.byteChan:
-
+	case res := <-o.byteChan:
+		return *res
 	// reuse existing buffer
 	default:
 		// create new buffer
-		return make([]byte, o.byteDataSize)
+		return make([]byte, o.ByteDataSize)
 	}
-	return
 }
 
 // PutByteSliceBuf reusable objects
-func (o *Option) PutByteSliceBuf(res []byte) {
-	res = make([]byte, o.byteDataSize)
+func (o *Option) PutByteSliceBuf(res *[]byte) {
+	*res = make([]byte, o.ByteDataSize)
 	select {
 	case o.byteChan <- res:
 	// reuse existing buffer
 	default:
+
 	}
 	return
 }
