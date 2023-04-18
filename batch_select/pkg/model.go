30ec8615d28197f209a13b0e1f804cfd9084705e
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 14:18:10 +0800

    Update model.go

--- batch_select/pkg/model.go
+++ batch_select/pkg/model.go
@@ -5,25 +5,25 @@ type MaxMinInfo struct {
 	Min string `gorm:"column:mi"`
 }
 
-//getMaxMinInfo Get the maximum and minimum values that meet the query criteria
-func (o *option) getMaxMinInfo() (maxMinData MaxMinInfo, err error) {
+// getMaxMinInfo Get the maximum and minimum values that meet the query criteria
+func (o *Option[T]) getMaxMinInfo() (maxMinData MaxMinInfo, err error) {
 	err =
 		o.mysqlCli.
 			Table(o.table).
-			Select("min(" + o.orderColumn + ") as mi," + "max(" + o.orderColumn + ") as ma").
-			Where(o.sqlWhere,o.whereCase...).
+			Select("min("+o.orderColumn+") as mi,"+"max("+o.orderColumn+") as ma").
+			Where(o.sqlWhere, o.whereCase...).
 			Take(&maxMinData).
 			Error
 	return
 }
 
-//getResInfo Get data according to conditions
-func (o *option) getResInfo(sqlWhere string,whereCase []interface{}) (res []map[string]interface{}, err error) {
-	res = make([]map[string]interface{}, 0, o.limit)
+// getResInfo Get data according to conditions
+func (o *Option[T]) getResInfo(sqlWhere string, whereCase []interface{}) (res []T, err error) {
+	res = make([]T, 0, o.limit)
 	err = o.mysqlCli.
 		Table(o.table).
 		Select(o.selectFiled).
-		Where(sqlWhere,whereCase...).
+		Where(sqlWhere, whereCase...).
 		//Limit(o.limit).
 		//Order(o.orderColumn).
 		Find(&res).
