1f79d19f53c7ad0d39871884d5b82b0e55264099
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 14:18:25 +0800

    Update option.go

--- batch_select/pkg/option.go
+++ batch_select/pkg/option.go
@@ -7,17 +7,17 @@ import (
 	"sync"
 )
 
-//EndFunc Functions that handle callbacks each time
-type EndFunc func(res *[]map[string]interface{})
+// EndFunc Functions that handle callbacks each time
+type EndFunc[T any] func(res *[]T)
 
-//SavePanic Functions that handle exception panic
-type SavePanic func(i interface{})
+// SavePanic Functions that handle exception panic
+type SavePanic func(i ...interface{})
 
-type Option interface {
-	apply(*option)
+type OptionInter[T any] interface {
+	apply(*Option[T])
 }
 
-type option struct {
+type Option[T any] struct {
 	debug bool
 	//OrderId         bool
 	//orderColumnIsTime         bool
@@ -32,10 +32,10 @@ type option struct {
 	whereCase      []interface{}
 	selectFiled    string
 	minWhereCh     chan *MinMaxInfo
-	resCh          chan *[]map[string]interface{}
-	resPool        chan []map[string]interface{}
+	resCh          chan *[]T
+	resPool        chan []T
 	err            []error
-	callFunc       EndFunc
+	callFunc       EndFunc[T]
 	savePanicFunc  SavePanic
 	wg             *sync.WaitGroup
 	revWg          *sync.WaitGroup
@@ -43,20 +43,20 @@ type option struct {
 	mysqlCli       *gorm.DB
 }
 
-type OpFunc func(*option)
+type OpFunc[T any] func(*Option[T])
 
-func NewOption(opt ...Option) *option {
-	return clone().WithOptions(opt...)
+func NewOption[T any](opt ...OptionInter[T]) *Option[T] {
+	return clone[T]().WithOptions(opt...)
 }
 
-//apply assignment function entity
-func (o OpFunc) apply(opt *option) {
+// apply assignment function entity
+func (o OpFunc[T]) apply(opt *Option[T]) {
 	o(opt)
 }
 
-//clone  new object
-func clone() *option {
-	return &option{
+// clone  new object
+func clone[T any]() *Option[T] {
+	return &Option[T]{
 		debug:          false,
 		limit:          Limit,
 		handleGoNum:    HandleGoNum,
@@ -78,8 +78,8 @@ func clone() *option {
 	}
 }
 
-//WithOptions Execute assignment function entity
-func (o *option) WithOptions(opt ...Option) *option {
+// WithOptions Execute assignment function entity
+func (o *Option[T]) WithOptions(opt ...OptionInter[T]) *Option[T] {
 	for _, v := range opt {
 		v.apply(o)
 	}
@@ -87,93 +87,93 @@ func (o *option) WithOptions(opt ...Option) *option {
 	return o
 }
 
-//initParams Initialization parameters
-func (o *option) initParams() {
-	o.resCh, o.resPool, o.minWhereCh = make(chan *[]map[string]interface{}, o.resChanSize), make(chan []map[string]interface{}, o.handleGoNum/3), make(chan *MinMaxInfo, o.handleGoNum)
+// initParams Initialization parameters
+func (o *Option[T]) initParams() {
+	o.resCh, o.resPool, o.minWhereCh = make(chan *[]T, o.resChanSize), make(chan []T, o.handleGoNum/3), make(chan *MinMaxInfo, o.handleGoNum)
 	if o.debug {
 		o.mysqlCli = o.mysqlCli.Debug()
 	}
 }
 
-//WithDebug debug mysql
-func WithDebug(debug bool) OpFunc {
-	return func(o *option) {
+// WithDebug debug mysql
+func WithDebug[T any](debug bool) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.debug = debug
 	}
 }
 
-//WithLimit How much to start processing default 100
-func WithLimit(size int) OpFunc {
-	return func(o *option) {
+// WithLimit How much to start processing default 100
+func WithLimit[T any](size int) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.limit = size
 	}
 }
 
-//WithHandleGoNum Number of goroutine processed default 100
-func WithHandleGoNum(num int) OpFunc {
-	return func(o *option) {
+// WithHandleGoNum Number of goroutine processed default 100
+func WithHandleGoNum[T any](num int) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.handleGoNum = num
 	}
 }
 
-//WithHandleRevGoNum Number of goroutine rev
-func WithHandleRevGoNum(num int) OpFunc {
-	return func(o *option) {
+// WithHandleRevGoNum Number of goroutine rev
+func WithHandleRevGoNum[T any](num int) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.handleRevGoNum = num
 	}
 }
 
-//WithTable will doing table name
-func WithTable(tableName string) OpFunc {
-	return func(o *option) {
+// WithTable will doing table name
+func WithTable[T any](tableName string) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.table = tableName
 	}
 }
 
-//WithOrderColumn order column
-func WithOrderColumn(name string) OpFunc {
-	return func(o *option) {
+// WithOrderColumn order column
+func WithOrderColumn[T any](name string) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.orderColumn = name
 	}
 }
 
-//WithSqlWhere where
-func WithSqlWhere(sqlWhere string, whereCase []interface{}) OpFunc {
-	return func(o *option) {
+// WithSqlWhere where
+func WithSqlWhere[T any](sqlWhere string, whereCase []interface{}) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.sqlWhere, o.whereCase = sqlWhere, whereCase
 	}
 }
 
-//WithSelectFiled select filed
-func WithSelectFiled(filed string) OpFunc {
-	return func(o *option) {
+// WithSelectFiled select filed
+func WithSelectFiled[T any](filed string) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.selectFiled = filed
 	}
 }
 
-//WithMysqlSqlCli where
-func WithMysqlSqlCli(cli *gorm.DB) OpFunc {
-	return func(o *option) {
+// WithMysqlSqlCli where
+func WithMysqlSqlCli[T any](cli *gorm.DB) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.mysqlCli = cli
 	}
 }
 
-//WithResChanSize rev chan size
-func WithResChanSize(size int) OpFunc {
-	return func(o *option) {
+// WithResChanSize rev chan size
+func WithResChanSize[T any](size int) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.resChanSize = size
 	}
 }
 
-//WithCallFunc callback end func
-func WithCallFunc(endHook EndFunc) OpFunc {
-	return func(o *option) {
+// WithCallFunc callback end func
+func WithCallFunc[T any](endHook EndFunc[T]) OpFunc[T] {
+	return func(o *Option[T]) {
 		o.callFunc = endHook
 	}
 }
 
-//defaultSavePanic
-func defaultSavePanic(i interface{}) {
+// defaultSavePanic
+func defaultSavePanic(i ...interface{}) {
 	if err := recover(); nil != err {
 		fmt.Println(i)
 		fmt.Println(err)
