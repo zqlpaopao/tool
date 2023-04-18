18dcbd0684a2f8b31c7ed873054baa25f615bc66
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 14:45:09 +0800

    Update common.go

--- batch_hook/pkg/common.go
+++ batch_hook/pkg/common.go
@@ -23,8 +23,6 @@ const (
 var (
 	ERRTaskNameIsEmpty = errors.New("task name is empty")
 	ERRHookFuncIsEmpty = errors.New("hookFunc is empty")
-	ErrNotHave         = errors.New("not have the task name")
-	ErrTaskClosed      = errors.New("this task has been closed")
 )
 
 // InitTaskModel Initialize task any
@@ -35,25 +33,6 @@ type InitTaskModel[T any] struct {
 
 // check Parameter detection
 func (s *InitTaskModel[T]) check() error {
-	if s.TaskName == "" {
-		return ERRTaskNameIsEmpty
-	}
-	return nil
-}
-
-// SubmitModel Submit task any
-type SubmitModel[T any] struct {
-	TaskName string
-	Data     []T
-}
-
-// SubmitItem Submit task parameters
-type SubmitItem struct {
-	Params []interface{}
-}
-
-// check Parameter detection
-func (s *SubmitModel[T]) check() error {
 	if s.TaskName == "" {
 		return ERRTaskNameIsEmpty
 	}
