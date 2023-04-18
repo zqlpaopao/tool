6d1270ab400c8b33a68ec25addd62f177ac60552
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 14:44:40 +0800

    Update batch_hook.go

--- batch_hook/pkg/batch_hook.go
+++ batch_hook/pkg/batch_hook.go
@@ -1,16 +1,13 @@
 package pkg
 
-import (
-	"sync/atomic"
-)
-
 type BatchHook[T any] struct {
-	batchInfo map[string]*OptionItem[T]
+	taskName string
+	batchOpt *OptionItem[T]
 }
 
 // NewBatchHook make new BatchHook
 func NewBatchHook[T any]() *BatchHook[T] {
-	return &BatchHook[T]{batchInfo: make(map[string]*OptionItem[T])}
+	return &BatchHook[T]{}
 }
 
 // InitTask Initialize batch processing hook
@@ -18,68 +15,34 @@ func (b *BatchHook[T]) InitTask(task InitTaskModel[T]) (err error) {
 	if err = task.check(); nil != err {
 		return
 	}
-	b.batchInfo[task.TaskName] = NewOption[T](task.Opt...)
+	b.taskName,
+		b.batchOpt =
+		task.TaskName,
+		NewOption[T](task.Opt...)
+
+	if b.taskName == "" {
+		return ERRTaskNameIsEmpty
+	}
 	return nil
 }
 
 // Submit Submit multiple tasks by name
-func (b *BatchHook[T]) Submit(items SubmitModel[T]) (err error) {
-	if _, ok := b.batchInfo[items.TaskName]; !ok {
-		return ErrNotHave
-	}
-	if b.batchInfo[items.TaskName].IsClose() {
-		return ErrTaskClosed
-	}
-	if err = b.batchInfo[items.TaskName].check(); nil != err {
-		return
-	}
-	for i := 0; i < len(items.Data); i++ {
-		b.batchInfo[items.TaskName].itemCh <- items.Data[i]
-	}
-	return
+func (b *BatchHook[T]) Submit(items *T) {
+	b.batchOpt.itemCh <- items
 }
 
 // Run Start multiple consumption tasks by name
-func (b *BatchHook[T]) Run(taskName string) error {
-	if _, ok := b.batchInfo[taskName]; !ok {
-		return ErrNotHave
-	}
-	if b.batchInfo[taskName].IsClose() {
-		return ErrTaskClosed
-	}
-	b.batchInfo[taskName].wg.Add(b.batchInfo[taskName].handleGoNum)
-	go b.batchInfo[taskName].Run()
-	return nil
+func (b *BatchHook[T]) Run() {
+	b.batchOpt.wg.Add(b.batchOpt.handleGoNum)
+	go b.batchOpt.Run()
 }
 
 // Release consumption go pool
-func (b *BatchHook[T]) Release(taskName string) error {
-	if _, ok := b.batchInfo[taskName]; !ok {
-		return ErrNotHave
-	}
-	if b.batchInfo[taskName].IsClose() {
-		return ErrTaskClosed
-	}
-	atomic.CompareAndSwapInt32(&b.batchInfo[taskName].close, OPENED, CLOSED)
-	close(b.batchInfo[taskName].itemCh)
-	return nil
+func (b *BatchHook[T]) Release() {
+	close(b.batchOpt.itemCh)
 }
 
-// WaitAll wait all
-func (b *BatchHook[T]) WaitAll() {
-	for _, v := range b.batchInfo {
-		v.wg.Wait()
-	}
-}
-
-// Wait wait one
-func (b *BatchHook[T]) Wait(taskName string) error {
-	if _, ok := b.batchInfo[taskName]; !ok {
-		return ErrNotHave
-	}
-	if b.batchInfo[taskName].IsClose() {
-		return ErrTaskClosed
-	}
-	b.batchInfo[taskName].wg.Wait()
-	return nil
+// Wait wait all
+func (b *BatchHook[T]) Wait() {
+	b.batchOpt.wg.Wait()
 }
