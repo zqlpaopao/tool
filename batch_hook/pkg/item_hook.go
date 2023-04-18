d409498d23e16f30214d90419e9e269c1975bc12
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 11:30:23 +0800

    Update item_hook.go

--- batch_hook/pkg/item_hook.go
+++ batch_hook/pkg/item_hook.go
@@ -7,57 +7,57 @@ import (
 	"time"
 )
 
-//IsClose state is close
-func (o *option) IsClose() bool {
+// IsClose state is close
+func (o *OptionItem[T]) IsClose() bool {
 	return atomic.LoadInt32(&o.close) == CLOSED
 }
 
-//check
-func (o *option) check() error {
+// check
+func (o *OptionItem[T]) check() error {
 	if o.hookFunc == nil {
 		return ERRHookFuncIsEmpty
 	}
 	return nil
 }
 
-//Run tidy other info
-func (o *option) Run() {
+// Run tidy other info
+func (o *OptionItem[T]) Run() {
 	//o.wg.Add(o.handleGoNum)
 	for i := 0; i < o.handleGoNum; i++ {
 		go o.doing()
 	}
 }
 
-//doing Handle by yourself every goroutine process
-func (o *option) doing() {
+// doing Handle by yourself every goroutine process
+func (o *OptionItem[T]) doing() {
 	var (
-		task   []interface{}
-		timer  = time.NewTimer(o.waitTime)
+		task  []T
+		timer = time.NewTimer(o.waitTime)
 		//timer1 = time.NewTimer(o.loopTime)
 	)
 	if o.savePanicFunc != nil {
-		defer o.savePanicFunc(task)
+		defer o.savePanicFunc(task...)
 	}
 	for {
 		select {
-		case v ,ok := <-o.itemCh:
-			if !ok{
-				if len(task)<1{
+		case v, ok := <-o.itemCh:
+			if !ok {
+				if len(task) < 1 {
 					goto END
 				}
 				o.hook(&task)
-				task = []interface{}{}
+				task = []T{}
 				goto END
 			}
 			task = append(task, v)
 			if len(task) >= o.doingSize {
 				o.hook(&task)
-				task = []interface{}{}
+				task = []T{}
 			}
 		case <-timer.C:
 			if len(task) > 0 {
 				o.hook(&task)
-				task = []interface{}{}
+				task = []T{}
 			}
 			timer.Reset(o.waitTime)
 		//case <-timer1.C:
@@ -78,8 +78,8 @@ END:
 	o.wg.Done()
 }
 
-//hook Execute specific functions
-func (o *option) hook(task *[]interface{}) {
+// hook Execute specific functions
+func (o *OptionItem[T]) hook(task *[]T) {
 	if o.endHook != nil {
 		o.endHook(o.hookFunc(*task))
 		return
@@ -87,8 +87,8 @@ func (o *option) hook(task *[]interface{}) {
 	o.hookFunc(*task)
 }
 
-//defaultSavePanic
-func defaultSavePanic(i interface{}) {
+// defaultSavePanic
+func defaultSavePanic[T any](i ...T) {
 	if err := recover(); nil != err {
 		fmt.Println(i)
 		fmt.Println(err)
