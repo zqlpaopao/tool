c4b2bc5443b997d3671c83c88678faa349b7dedf
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 14:17:15 +0800

    Update common.go

--- batch_select/pkg/common.go
+++ batch_select/pkg/common.go
@@ -1,38 +1,169 @@
 package pkg
 
 import (
-	"errors"
+	"strconv"
+	"strings"
+	"time"
 )
 
-const (
-	Limit       = 10000
-	HandleGoNum = 10
-	ChanSize    = 10000
-)
-
-var (
-	ERRTaskNameIsEmpty          = errors.New("task name is empty")
-	ERRMySqlCli                 = errors.New("mysql cli is empty")
-	ERROrderColumn              = errors.New("order column is empty")
-	ErrNotHave                  = errors.New("not have the task name")
-	ErrSelectNotHaveOrderColumn = errors.New("the query column has no columns to sort")
-)
-
-// InitTaskModel Initialize task model
-type InitTaskModel[T any] struct {
-	TaskName string
-	Opt      []OptionInter[T]
-}
-
-type MinMaxInfo struct {
-	MinId string
-	MaxId string
-}
-
-// check Parameter detection
-func (s *InitTaskModel[T]) check() error {
-	if s.TaskName == "" {
-		return ERRTaskNameIsEmpty
+// check verification mysql client and order column
+func (o *Option[T]) check() error {
+	if o.mysqlCli == nil {
+		return ERRMySqlCli
+	}
+	if o.orderColumn == "" {
+		return ERROrderColumn
+	}
+	if o.selectFiled != "*" && !strings.Contains(o.selectFiled, o.orderColumn) {
+		return ErrSelectNotHaveOrderColumn
 	}
 	return nil
+}
+
+// Run tidy other info
+func (o *Option[T]) Run() {
+	o.getBaseData()
+	go o.producer()
+	o.consumer()
+}
+
+// getBaseData get base data ,will run all
+func (o *Option[T]) getBaseData() {
+	var (
+		maxBaseInfo MaxMinInfo
+		err         error
+	)
+	defer o.savePanicFunc(o.table)
+	if maxBaseInfo, err = o.getMaxMinInfo(); nil != err {
+		panic(err)
+	}
+	if maxBaseInfo == (MaxMinInfo{}) {
+		close(o.minWhereCh)
+		return
+	}
+	if err = o.OrderIdDebug(&maxBaseInfo); nil != err {
+		panic(err)
+	}
+	return
+}
+
+// OrderIdDebug If it is obtained according to the self increasing ID, create an acquisition interval
+func (o *Option[T]) OrderIdDebug(maxBaseInfo *MaxMinInfo) (err error) {
+	var minId, maxId, loop int
+	if minId, err = strconv.Atoi(maxBaseInfo.Min); nil != err {
+		return
+	}
+	if maxId, err = strconv.Atoi(maxBaseInfo.Max); nil != err {
+		return
+	}
+	loop = (maxId - minId + o.limit) / o.limit
+	go o.sendMinWhere(minId, loop)
+	return
+}
+
+// sendMinWhere Send to producer
+func (o *Option[T]) sendMinWhere(minId, loop int) {
+	for i := 1; i <= loop; i++ {
+		info := &MinMaxInfo{
+			MinId: strconv.Itoa(minId),
+			MaxId: strconv.Itoa(minId + o.limit),
+		}
+		o.minWhereCh <- info
+		minId = minId + o.limit
+	}
+	close(o.minWhereCh)
+}
+
+// doing Handle by yourself every goroutine process
+func (o *Option[T]) producer() {
+	o.wg.Add(o.handleGoNum)
+	for i := 0; i < o.handleGoNum; i++ {
+		go o.producerLoopWorker()
+	}
+	o.wg.Wait()
+	close(o.resCh)
+}
+
+// producerLoopWorker Producers obtain production interval data
+func (o *Option[T]) producerLoopWorker() {
+	defer o.savePanicFunc("producerLoopWorker-" + o.table)
+LABEL:
+	for {
+		select {
+		case v, ok := <-o.minWhereCh:
+			if !ok {
+				break LABEL
+			}
+			if v == nil {
+				continue
+			}
+			o.producerDbData(v)
+		default:
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+	o.wg.Done()
+}
+
+// getRes get every item result
+func (o *Option[T]) producerDbData(v *MinMaxInfo) {
+	var (
+		sqlStr = ""
+		err    error
+		res    = o.getResMap()
+	)
+	sqlStr = o.orderColumn + " >= '" + v.MinId + "' and " + o.orderColumn + " < " + "'" + v.MaxId + "'"
+	if "" != strings.Trim(o.sqlWhere, " ") {
+		sqlStr += " and " + o.sqlWhere
+	}
+	if res, err = o.getResInfo(sqlStr, o.whereCase); nil != err {
+		o.retryFind(v, err)
+		return
+	}
+	if len(res) < 1 {
+		return
+	}
+	o.resCh <- &res
+}
+
+// retryFind put back the min id
+func (o *Option[T]) retryFind(id *MinMaxInfo, err error) {
+	if id != nil {
+		o.minWhereCh <- id
+	}
+	if nil != err {
+		o.err = append(o.err, err)
+	}
+}
+
+// consumer
+func (o *Option[T]) consumer() {
+	o.revWg.Add(o.handleRevGoNum)
+	for i := 0; i < o.handleRevGoNum; i++ {
+		go o.revResCh()
+	}
+	o.revWg.Wait()
+	o.wgAll.Done()
+}
+
+// revResCh tidy the result and call back func
+func (o *Option[T]) revResCh() {
+	defer o.savePanicFunc("revResCh-" + o.table)
+LABEL:
+	for {
+		select {
+		case v, ok := <-o.resCh:
+			if !ok {
+				break LABEL
+			}
+			if o.callFunc != nil && v != nil && len(*v) > 0 {
+				o.callFunc(v)
+				*v = make([]T, 0, o.limit+1)
+				o.putResMap(*v)
+			}
+		default:
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+	o.revWg.Done()
 }
