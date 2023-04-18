de114951c7f79a7856aaca818d260b74a3f3821d
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 14:15:48 +0800

    Update common.go

--- batch_select/pkg/common.go
+++ batch_select/pkg/common.go
@@ -2,35 +2,58 @@ package pkg
 
 import (
 	"errors"
+	"time"
 )
 
 const (
-	Limit       = 10000
+	DoingSize   = 1000
 	HandleGoNum = 10
-	ChanSize    = 10000
+	ChanSize    = 1000
+	WaitTime    = 2 * time.Second
+	LoopTime    = 1 * time.Second
+)
+
+const (
+	// OPENED represents that the pool is opened.
+	OPENED = iota
+	// CLOSED represents that the pool is closed.
+	CLOSED
 )
 
 var (
-	ERRTaskNameIsEmpty          = errors.New("task name is empty")
-	ERRMySqlCli                 = errors.New("mysql cli is empty")
-	ERROrderColumn              = errors.New("order column is empty")
-	ErrNotHave                  = errors.New("not have the task name")
-	ErrSelectNotHaveOrderColumn = errors.New("the query column has no columns to sort")
+	ERRTaskNameIsEmpty = errors.New("task name is empty")
+	ERRHookFuncIsEmpty = errors.New("hookFunc is empty")
+	ErrNotHave         = errors.New("not have the task name")
+	ErrTaskClosed      = errors.New("this task has been closed")
 )
 
-//InitTaskModel Initialize task model
-type InitTaskModel struct {
+// InitTaskModel Initialize task any
+type InitTaskModel[T any] struct {
 	TaskName string
-	Opt      []Option
+	Opt      []Option[T]
 }
 
-type MinMaxInfo struct {
-	MinId string
-	MaxId string
+// check Parameter detection
+func (s *InitTaskModel[T]) check() error {
+	if s.TaskName == "" {
+		return ERRTaskNameIsEmpty
+	}
+	return nil
 }
 
-//check Parameter detection
-func (s *InitTaskModel) check() error {
+// SubmitModel Submit task any
+type SubmitModel[T any] struct {
+	TaskName string
+	Data     []T
+}
+
+// SubmitItem Submit task parameters
+type SubmitItem struct {
+	Params []interface{}
+}
+
+// check Parameter detection
+func (s *SubmitModel[T]) check() error {
 	if s.TaskName == "" {
 		return ERRTaskNameIsEmpty
 	}
