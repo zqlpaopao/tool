c95c206fd3ee2072f44e702bd2fa8aca4df1d24c
code <43371021+zqlpaopao@users.noreply.github.com>
2023-04-18 11:29:12 +0800

    Update main.go

--- batch_hook/main.go
+++ batch_hook/main.go
@@ -12,113 +12,83 @@ import (
 )
 
 type User struct {
-	Name        string
-	Role        string
-	Age         int
+	Name   string
+	Role   string
+	Age    int
 	Salary int
 }
 
+func (u User) Self() {
+
+}
+
+type Default struct {
+	Name   string
+	Role   string
+	Age    int
+	Salary int
+}
+
+func (d Default) Self() {
+
+}
 
 func main() {
 
-	b := pkg.NewBatchHook()
+	b := pkg.NewBatchHook[User]()
 
-	task := []pkg.InitTaskModel{
-		{
-			TaskName: "test1",
-			Opt: []pkg.Option{
-				pkg.WithWaitTime(3 * time.Second),
-				pkg.WithLoopTime(5 * time.Second),
-				pkg.WithChanSize(1000),
-				pkg.WithDoingSize(10),
-				pkg.WithHandleGoNum(3),
-				pkg.WithHookFunc(func(i []interface{}) bool {
-					fmt.Println("=============", len(i))
-					fmt.Println("=============", i)
+	task := pkg.InitTaskModel[User]{
+		TaskName: "test1",
+		Opt: []pkg.Option[User]{
+			pkg.WithWaitTime[User](3 * time.Second),
+			pkg.WithLoopTime[User](5 * time.Second),
+			pkg.WithChanSize[User](1000),
+			pkg.WithDoingSize[User](10),
+			pkg.WithHandleGoNum[User](3),
+			pkg.WithHookFunc[User](func(i []User) bool {
+				fmt.Println("=============", len(i))
+				fmt.Println("=============", i)
 
-					for _, vl := range i {
-						if v1, ok := vl.(User); ok {
-							fmt.Println(v1)
-						} else {
-							os.Exit(1)
-						}
-					}
-					return true
-				}),
-				pkg.WithEndHook(func(b bool, i ...interface{}) {
-					fmt.Println("--------------")
-					fmt.Println(b)
-					fmt.Println(len(i))
-					fmt.Println(i)
-				}),
-				pkg.WithSavePanic(func(i interface{}) {
-					if err := recover(); err != nil {
-						fmt.Println(err)
-						fmt.Println(string(debug.Stack()))
-						os.Exit(8)
-					}
-				}),
-			},
-		},
-		{
-			TaskName: "test2",
-			Opt: []pkg.Option{
-				pkg.WithWaitTime(3 * time.Second),
-				pkg.WithLoopTime(5 * time.Second),
-				pkg.WithChanSize(1000),
-				pkg.WithDoingSize(10),
-				pkg.WithHandleGoNum(3),
-				pkg.WithHookFunc(func(i []interface{}) bool {
-					fmt.Println("=============", len(i))
-					fmt.Println("=============", i)
-
-					for _, vl := range i {
-						if v1, ok := vl.(User); ok {
-							fmt.Println(v1)
-						} else {
-							os.Exit(1)
-						}
-					}
-					return true
-				}),
-				pkg.WithEndHook(func(b bool, i ...interface{}) {
-					fmt.Println("--------------")
-					fmt.Println(b)
-					fmt.Println(len(i))
-					fmt.Println(i)
-				}),
-				pkg.WithSavePanic(func(i interface{}) {
-					if err := recover(); err != nil {
-						fmt.Println(err)
-						fmt.Println(string(debug.Stack()))
-						os.Exit(9)
-
-					}
-				}),
-			},
+				for _, vl := range i {
+					fmt.Println(vl.Age, vl.Role, vl.Salary, vl.Name)
+				}
+				return true
+			}),
+			pkg.WithEndHook[User](func(b bool, i ...User) {
+				fmt.Println("--------------")
+				fmt.Println(b)
+				fmt.Println(len(i))
+				fmt.Println(i)
+			}),
+			pkg.WithSavePanic[User](func(i ...User) {
+				if err := recover(); err != nil {
+					fmt.Println(err)
+					fmt.Println(string(debug.Stack()))
+					os.Exit(8)
+				}
+			}),
 		},
 	}
 
-	if err := b.InitTask(task...); nil != err {
+	if err := b.InitTask(task); nil != err {
 		fmt.Println(err, "============")
 		os.Exit(1)
 	}
 
-	if err := b.Run([]string{"test1", "test2"}...); nil != err {
+	if err := b.Run("test1"); nil != err {
 		fmt.Println(err)
 		os.Exit(4)
 	}
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 	go func() {
 		i1 := 0
 		for {
 			if i1 > 20 {
 				goto END
 			}
-			item := pkg.SubmitModel{
+			item := pkg.SubmitModel[User]{
 				TaskName: "test1",
-
 			}
 			for i := 0; i < 5; i++ {
 				i1++
@@ -127,7 +97,7 @@ func main() {
 				user.Age = user.Age + i
 				user.Name = user.Name + strconv.Itoa(i1)
 				user.Role = user.Role + strconv.Itoa(i1)
-				item.Data = append(item.Data, &user)
+				item.Data = append(item.Data, user)
 				//item.Item.Params = append(item.Item.Params,user)
 				time.Sleep(time.Second)
 
@@ -139,38 +109,6 @@ func main() {
 		}
 	END:
 		wg.Done()
-	}()
-
-	go func() {
-		i1 := 0
-
-		for {
-			if i1 > 20 {
-				goto END
-			}
-			item := pkg.SubmitModel{
-				TaskName: "test2",
-			}
-			for i := 0; i < 5; i++ {
-				i1++
-				user := User{Name: "ZHANGSAN", Age: 0, Role: "ZHANGSAN", Salary: 300000}
-
-				user.Age = user.Age + i
-				user.Name = user.Name + strconv.Itoa(i1)
-				user.Role = user.Role + strconv.Itoa(i1)
-				item.Data = append(item.Data, &user)
-				//item.Item.Params = append(item.Item.Params,user)
-				time.Sleep(time.Second)
-
-			}
-			if err := b.Submit(item); nil != err {
-				fmt.Println(err)
-				os.Exit(2)
-			}
-		}
-	END:
-		wg.Done()
-
 	}()
 
 	go func() {
@@ -182,19 +120,15 @@ func main() {
 
 	time.Sleep(20 * time.Second)
 	wg.Wait()
-	if err := b.Release([]string{"test1", "test2"}...); nil != err {
+	if err := b.Release("test1"); nil != err {
 		fmt.Println(err)
 		os.Exit(21)
 	}
-
 
 	b.WaitAll()
 	//_ = b.Wait([]string{"test1", "test2"}...)
 	fmt.Println(";;;;;;;;;;;;;;;;;;;;;;;;;")
 
-
-
 	fmt.Println("结束")
-
 
 }
