6ef1d7947c8993f1d6bc07addb9a028218950994
code <43371021+zqlpaopao@users.noreply.github.com>
2023-06-27 11:00:47 +0800

    Create time.go

---
+++ unit/pkg/time.go
@@ -0,0 +1,43 @@
+package pkg
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+	"strconv"
+)
+
+var timeSizes = []string{"ns", "us", "ms", "s"}
+
+// humanizeTimes
+//
+// 82 854 982 -> 82.855 ms
+func humanizeTimes(s uint64, long int, base uint, sizes []string) string {
+	if s < uint64(base) {
+		return fmt.Sprintf("%d B", s)
+	}
+
+	num, _ := new(big.Float).SetPrec(base).SetString(strconv.FormatUint(s, 10))
+
+	e := math.Floor(logN(float64(s), float64(base)))
+	denominator := big.NewFloat(math.Pow(float64(base), e))
+
+	suffix := timeSizes[int(e)]
+
+	f := "%.2f %s"
+	if long < 4 {
+		f = FPix[long]
+	}
+
+	denominator = num.Quo(num, denominator)
+
+	return fmt.Sprintf(f, denominator, suffix)
+}
+
+// Times produces a humanizeBytes readable representation of an SI size.
+//
+// Duration(82854982) -> 82.855 ms
+// Duration(82814982) -> 82.81 ms
+func Times(s uint64, long int) string {
+	return humanizeTimes(s, long, 1000, timeSizes)
+}
