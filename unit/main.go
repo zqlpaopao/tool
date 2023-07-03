bcb0d368901224f9dcbd4f4365a1eecbb7b85e93
code <43371021+zqlpaopao@users.noreply.github.com>
2023-07-03 17:42:22 +0800

    Update main.go

--- unit/main.go
+++ unit/main.go
@@ -7,16 +7,23 @@ import (
 
 func main() {
 	//fmt.Println(pkg.IBytes(82814982, 3))
-	fmt.Println(pkg.Times(82854982, 3))
-	fmt.Println(pkg.Times(82814982, 2))
-
-	fmt.Println(pkg.IBytes(82854982, 3))
-	fmt.Println(pkg.IBytes(82854982, 2))
-	fmt.Println(pkg.IBytes(82854982, 0))
-
-	fmt.Println(pkg.Bytes(82854982, 3))
-	fmt.Println(pkg.Bytes(82854982, 2))
-	fmt.Println(pkg.Bytes(82854982, 0))
+	//fmt.Println(pkg.Times(82854982, 3))
+	////fmt.Println(pkg.Times(1750, 2))
+	fmt.Println(1755, pkg.TimesHalfAdjust(1755, 2))
+	fmt.Println(1759, pkg.TimesHalfAdjust(1759, 2))
+	fmt.Println(1756, pkg.TimesHalfAdjust(1756, 2))
+	fmt.Println(1750, pkg.TimesHalfAdjust(1750, 2))
+	fmt.Println(1751, pkg.TimesHalfAdjust(1751, 2))
+	fmt.Println(17517, pkg.TimesHalfAdjust(1751789, 3))
+	//fmt.Println(pkg.TimesHalfAdjust(1755, 2))
+	//
+	//fmt.Println(pkg.IBytes(82854982, 3))
+	//fmt.Println(pkg.IBytes(82854982, 2))
+	//fmt.Println(pkg.IBytes(82854982, 0))
+	//
+	//fmt.Println(pkg.Bytes(82854982, 3))
+	//fmt.Println(pkg.Bytes(82854982, 2))
+	//fmt.Println(pkg.Bytes(82854982, 0))
 
 	//Bytes(82854982) -> 83 MB
 }
