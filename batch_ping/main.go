ae48ab082b3ba746661b297277798a4c24a0d845
code <43371021+zqlpaopao@users.noreply.github.com>
2023-08-01 11:10:11 +0800

    Update main.go

--- batch_ping/main.go
+++ batch_ping/main.go
@@ -2,44 +2,75 @@ package main
 
 import (
 	"fmt"
+	cmap "github.com/orcaman/concurrent-map/v2"
 	"github.com/zqlpaopao/tool/batch_ping/pkg"
+	"os"
+	"sync"
 	"time"
 )
 
+type data struct {
+	lock *sync.RWMutex
+	info *pkg.PingItem
+}
+
+var Info = cmap.New[*data]()
+
 func main() {
-	p := pkg.NewPingPoolWithOptions()
-	p.Run()
+	p := pkg.NewPingPoolWithOptions(
+		pkg.WithOnRevFunc(func(ping *pkg.PingItem, packet *pkg.Packet) {
+			fmt.Println("ID", ping.ID())
+			fmt.Println("Seq", packet.Seq)
+			var (
+				p  *data
+				ok bool
+			)
+			if p, ok = Info.Get(ping.Addr); !ok {
+				return
+			}
+			p.lock.Lock()
+			if p.info.PacketsRev == p.info.Count {
+				p.info.PacketsSent = p.info.Count
+				ps := p.info.Statistics()
+				pkg.Log(ps)
+				p.info.ResetRttS()
+				//
+			} else {
+				p.info.PacketsRev++
+				p.info.RttS = append(p.info.RttS, packet.EndTime.Sub(packet.StartTime))
+			}
+			//
+			p.lock.Unlock()
+
+		}),
+		pkg.WithErrCallBack(func(ping *pkg.PingItem, err error) {
+			if err == nil {
+				return
+			}
+			fmt.Println(ping.Addr, err)
+		}),
+	).Run()
 	err := p.Error()
 	if err != nil {
 		panic(err)
 	}
+	var i = 0
 
-	//err = p.Submit("baidu.com")
+	for i < 10 {
+		pushPingItem(p, "baidu.com")
+		pushPingItem(p, "qq.com")
+		//pushAddr(p, "baidu.com")
+		//pushAddr(p, "qq.com")
 
-	for {
-		item := &pkg.PingItem{
-			Interval:    100 * time.Millisecond,
-			Timeout:     3 * time.Second,
-			Count:       10,
-			PacketsSent: 0,
-			PacketsRev:  0,
-			OnFinish:    nil,
-			Size:        80,
-			Tracker:     1897654345,
-			Source:      "",
-			Addr:        "jd.com",
-		}
-		err = item.PingWith()
-		if err != nil {
-			fmt.Println(err)
-		}
-		err = p.SubmitPingItem(item)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		time.Sleep(1 * time.Second)
+		i++
 	}
+	p.Close()
+	p.CloseRev()
+
+	//for {
+	//
+	//	time.Sleep(1 * time.Second)
+	//}
 
 	//time.Sleep(30 * time.Second)
 	//p.Close()
@@ -47,7 +78,55 @@ func main() {
 	//
 	//p.Wait()
 
-	fmt.Println("end")
+}
 
-	select {}
+func pushPingItem(p *pkg.PingPool, addr string) {
+	var err error
+	item := &pkg.PingItem{
+		Interval:    100 * time.Millisecond,
+		Timeout:     3 * time.Second,
+		Count:       3,
+		PacketsSent: 0,
+		PacketsRev:  0,
+		Size:        800,
+		Tracker:     1897654345,
+		Source:      "",
+		Addr:        addr,
+	}
+
+	if _, ok := Info.Get(item.Addr); !ok {
+		Info.Set(item.Addr, &data{
+			lock: &sync.RWMutex{},
+			info: item,
+		})
+	}
+
+	err = item.PingWith()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = p.SubmitPingItem(item)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func pushAddr(p *pkg.PingPool, addr string) {
+	var (
+		err error
+		ps  *pkg.PingItem
+	)
+
+	if ps, err = p.Submit(addr); nil != err {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if _, ok := Info.Get(addr); !ok {
+		Info.Set(addr, &data{
+			lock: &sync.RWMutex{},
+			info: ps,
+		})
+	}
 }
