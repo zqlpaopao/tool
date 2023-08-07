ebb0b15f6a4d156c735f58cb08d3c4622b331161
code <43371021+zqlpaopao@users.noreply.github.com>
2023-08-07 16:05:39 +0800

    Add files via upload

---
+++ batch_ping_agent/pkg/packetconn.go
@@ -0,0 +1,187 @@
+package ping
+
+import (
+	"net"
+	"runtime"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
+type packetConn interface {
+	Close() error
+	ICMPRequestType() icmp.Type
+	ReadFrom(b []byte) (n int, ttl int, src net.Addr, err error)
+	SetFlagTTL() error
+	SetReadDeadline(t time.Time) error
+	WriteTo(b []byte, dst net.Addr) (int, error)
+	SetTTL(ttl int)
+	SetMark(m uint) error
+	SetDoNotFragment() error
+}
+
+type icmpConn struct {
+	c   *icmp.PacketConn
+	ttl int
+}
+
+func (c *icmpConn) Close() error {
+	return c.c.Close()
+}
+
+func (c *icmpConn) SetTTL(ttl int) {
+	c.ttl = ttl
+}
+
+func (c *icmpConn) SetReadDeadline(t time.Time) error {
+	return c.c.SetReadDeadline(t)
+}
+
+func (c *icmpConn) WriteTo(b []byte, dst net.Addr) (int, error) {
+	if c.c.IPv6PacketConn() != nil {
+		if err := c.c.IPv6PacketConn().SetHopLimit(c.ttl); err != nil {
+			return 0, err
+		}
+	}
+	if c.c.IPv4PacketConn() != nil {
+		if err := c.c.IPv4PacketConn().SetTTL(c.ttl); err != nil {
+			return 0, err
+		}
+	}
+
+	return c.c.WriteTo(b, dst)
+}
+
+type icmpV4Conn struct {
+	icmpConn
+}
+
+func (c *icmpV4Conn) SetFlagTTL() error {
+	err := c.c.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true)
+	if runtime.GOOS == "windows" {
+		return nil
+	}
+	return err
+}
+
+func (c *icmpV4Conn) ReadFrom(b []byte) (int, int, net.Addr, error) {
+	ttl := -1
+	n, cm, src, err := c.c.IPv4PacketConn().ReadFrom(b)
+	if cm != nil {
+		ttl = cm.TTL
+	}
+	return n, ttl, src, err
+}
+
+func (c icmpV4Conn) ICMPRequestType() icmp.Type {
+	return ipv4.ICMPTypeEcho
+}
+
+type icmpV6Conn struct {
+	icmpConn
+}
+
+func (c *icmpV6Conn) SetFlagTTL() error {
+	err := c.c.IPv6PacketConn().SetControlMessage(ipv6.FlagHopLimit, true)
+	if runtime.GOOS == "windows" {
+		return nil
+	}
+	return err
+}
+
+func (c *icmpV6Conn) ReadFrom(b []byte) (int, int, net.Addr, error) {
+	ttl := -1
+	n, cm, src, err := c.c.IPv6PacketConn().ReadFrom(b)
+	if cm != nil {
+		ttl = cm.HopLimit
+	}
+	return n, ttl, src, err
+}
+
+func (c icmpV6Conn) ICMPRequestType() icmp.Type {
+	return ipv6.ICMPTypeEchoRequest
+}
+
+func (p *Pool) getConn4() (res packetConn, err error) {
+TRY:
+	select {
+	case res = <-p.conn4:
+	// reuse existing buffer
+	default:
+		// create new buffer
+		var i int
+		res, err = p.makeConn4()
+		if (res == nil || err != nil) && i < 3 {
+			i++
+			goto TRY
+		}
+		return
+	}
+	return
+}
+func (p *Pool) setConn4(conn packetConn) {
+	select {
+	case p.conn4 <- conn:
+	// reuse existing buffer
+	default:
+	}
+}
+func (p *Pool) getConn6() (res packetConn, err error) {
+TRY:
+	select {
+	case res = <-p.conn6:
+	// reuse existing buffer
+	default:
+		// create new buffer
+		var i int
+		res, err = p.makeConn6()
+		if (res == nil || err != nil) && i < 3 {
+			i++
+			goto TRY
+		}
+		return
+	}
+	return
+}
+
+func (p *Pool) setConn6(conn packetConn) {
+	select {
+	case p.conn6 <- conn:
+	// reuse existing buffer
+	default:
+	}
+}
+
+func (p *Pool) makeConn4() (packetConn, error) {
+	var (
+		c4  icmpV4Conn
+		err error
+	)
+
+	if c4.c, err = icmp.ListenPacket(ipv4Proto[p.option.protocol], p.option.source); nil != err {
+		return nil, err
+	}
+	//
+	if err = c4.SetFlagTTL(); nil != err {
+		return nil, err
+	}
+	return &c4, nil
+}
+
+func (p *Pool) makeConn6() (packetConn, error) {
+	var (
+		c6  icmpV6Conn
+		err error
+	)
+
+	if c6.c, err = icmp.ListenPacket(ipv6Proto[p.option.protocol], p.option.source); nil != err {
+		return nil, err
+	}
+	if err = c6.SetFlagTTL(); nil != err {
+		return nil, err
+	}
+
+	return &c6, nil
+}
---
+++ batch_ping_agent/pkg/ping.go
@@ -0,0 +1,345 @@
+package ping
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"math/rand"
+	"net"
+	"sync/atomic"
+	"time"
+)
+
+// New returns a new ping struct pointer.
+func New(addr string) *Ping {
+	return &Ping{
+		Size:     timeSliceLength + trackerLength,
+		addr:     addr,
+		id:       0,
+		ipaddr:   nil,
+		ipv4:     false,
+		network:  IP,
+		protocol: Udp,
+		TTL:      64,
+		uuid:     uuid.New(),
+	}
+}
+
+// NewPing returns a new ping and resolves the address.
+func NewPing(addr string) (*Ping, error) {
+	p := New(addr)
+	return p, p.Resolve()
+}
+
+// Ping represents a packet sender/receiver.
+type Ping struct {
+	ipaddr *net.IPAddr
+	addr   string
+
+	// protocol is "icmp" or "udp".
+	protocol string
+
+	// network is one of "ip", "ip4", or "ip6".
+	network string
+
+	// Size of packet being sent
+	Size int
+
+	sequence int
+
+	// Tracker: Used to uniquely identify packets - Deprecated
+	Tracker uint64
+
+	// mark is a SO_MARK (fw-mark) set on outgoing icmp packets
+	mark uint
+
+	TTL  int
+	id   int
+	uuid uuid.UUID
+
+	ipv4 bool
+
+	// df when true sets the do-not-fragment bit in the outer IP or IPv6 header
+	df bool
+}
+
+type packet struct {
+	addr   net.Addr
+	bytes  []byte
+	nBytes int
+	ttl    int
+	ipv4   bool
+}
+
+// Packet represents a received and processed ICMP echo packet.
+type Packet struct {
+	StartTime time.Time
+	EndTime   time.Time
+	IPAddr    *net.IPAddr
+	Addr      string
+	uuid      string
+	Rtt       time.Duration
+	NBytes    int
+	Seq       int
+	TTL       int
+	ID        int
+}
+
+// SetIPAddrCheckIpv4 sets the ip address of the target host.
+func (p *Ping) SetIPAddrCheckIpv4(ipaddr *net.IPAddr) *Ping {
+	p.ipv4 = isIPv4(ipaddr.IP)
+
+	p.ipaddr = ipaddr
+	p.addr = ipaddr.String()
+
+	return p
+}
+
+// SetUUid returns the ip address of the target host.
+func (p *Ping) SetUUid(uuid uuid.UUID) *Ping {
+	p.uuid = uuid
+	return p
+}
+
+// Resolve does the DNS lookup for the Ping address and sets IP protocol.
+func (p *Ping) Resolve() error {
+	if len(p.addr) == 0 {
+		return errors.New("addr cannot be empty")
+	}
+	ipaddr, err := net.ResolveIPAddr(p.network, p.addr)
+	if err != nil {
+		return err
+	}
+
+	p.ipv4 = isIPv4(ipaddr.IP)
+
+	p.ipaddr = ipaddr
+
+	return nil
+}
+
+// SetAddr resolves and sets the ip address of the target host, addr can be a
+// DNS name like "www.google.com" or IP like "127.0.0.1".
+func (p *Ping) SetAddr(addr string) error {
+	oldAddr := p.addr
+	p.addr = addr
+	err := p.Resolve()
+	if err != nil {
+		p.addr = oldAddr
+		return err
+	}
+	return nil
+}
+
+// SetDstAddr set packet number
+func (p *Ping) SetDstAddr(addr string) *Ping {
+	p.addr = addr
+	return p
+}
+
+// SetSize set packet number
+func (p *Ping) SetSize(size int) *Ping {
+	p.Size = size
+	return p
+}
+
+// SetTtl set packet ttl
+func (p *Ping) SetTtl(ttl int) *Ping {
+	p.TTL = ttl
+	return p
+}
+
+// SetIpV4 set the ip is ipv4
+func (p *Ping) SetIpV4() *Ping {
+	p.ipv4 = true
+	return p
+}
+
+// SetResolveIpAddr set the ip is ipaddr
+func (p *Ping) SetResolveIpAddr(ipaddr *net.IPAddr) *Ping {
+	p.ipaddr = ipaddr
+	return p
+}
+
+// SetNetwork allows configuration of DNS resolution.
+// * "ip" will automatically select IPv4 or IPv6.
+// * "ip4" will select IPv4.
+// * "ip6" will select IPv6.
+func (p *Ping) SetNetwork(n string) *Ping {
+	switch n {
+	case "ip4":
+		p.network = "ip4"
+	case "ip6":
+		p.network = "ip6"
+	default:
+		p.network = "ip"
+	}
+	return p
+}
+
+// SetPrivileged sets the type of ping will send.
+// false means ping will send an "unprivileged" UDP ping.
+// true means ping will send a "privileged" raw ICMP ping.
+// NOTE: setting to true requires that it be run with super-user privileges.
+func (p *Ping) SetPrivileged(privileged bool) *Ping {
+	if privileged {
+		p.protocol = "icmp"
+	} else {
+		p.protocol = "udp"
+	}
+	return p
+}
+
+// SetMark sets a mark intended to be set on outgoing ICMP packets.
+func (p *Ping) SetMark(m uint) *Ping {
+	p.mark = m
+	return p
+}
+
+// SetID sets the ICMP identifier.
+func (p *Ping) SetID(id int) *Ping {
+	p.id = id
+	return p
+}
+
+// SetDoNotFragment sets the do-not-fragment bit in the outer IP header to the desired value.
+func (p *Ping) SetDoNotFragment(df bool) *Ping {
+	p.df = df
+	return p
+}
+
+///////////////////////////////////////////////// get /////////////////////////////////////
+
+// Privileged returns whether ping is running in privileged mode.
+func (p *Ping) Privileged() bool {
+	return p.protocol == "icmp"
+}
+
+// Addr returns the string ip address of the target host.
+func (p *Ping) Addr() string {
+	return p.addr
+}
+
+// IPAddr returns the ip address of the target host.
+func (p *Ping) IPAddr() *net.IPAddr {
+	return p.ipaddr
+}
+
+// ID returns the ICMP identifier.
+func (p *Ping) ID() int {
+	return p.id
+}
+
+// Mark returns the mark to be set on outgoing ICMP packets.
+func (p *Ping) Mark() uint {
+	return p.mark
+}
+
+// Statistics /////////////////////////////////////////////// Statistics /////////////////////////////////////
+// Statistics represent the stats of a currently running or finished
+// ping operation.
+type Statistics struct {
+	IPAddr         *net.IPAddr
+	Addr           string
+	RttS           []time.Duration
+	PacketsReceive int
+	PacketsSent    int
+	PacketLoss     float64
+	MinRtt         time.Duration
+	MaxRtt         time.Duration
+	AvgRtt         time.Duration
+	StdDevRtt      time.Duration
+}
+
+// StatisticsLog returns the statistics of the ping. This can be run while the
+// ping is running, or after it is finished. OnFinish calls this function to
+// get it's finished statistics.
+func StatisticsLog(ps *Ping, packet *Packet) *Statistics {
+	loss, rtt := 0.00, packet.EndTime.Sub(packet.StartTime)
+	s := Statistics{
+		PacketsSent:    1,
+		PacketsReceive: 1,
+		PacketLoss:     loss,
+		RttS:           []time.Duration{rtt},
+		Addr:           ps.Addr(),
+		IPAddr:         ps.ipaddr,
+		MaxRtt:         rtt,
+		MinRtt:         rtt,
+		AvgRtt:         rtt,
+	}
+	return &s
+}
+
+type expBackoff struct {
+	baseDelay time.Duration
+	maxExp    int64
+	c         int64
+}
+
+func (b *expBackoff) Get() time.Duration {
+	if b.c < b.maxExp {
+		b.c++
+	}
+
+	return b.baseDelay * time.Duration(rand.Int63n(1<<b.c))
+}
+
+func newExpBackoff(baseDelay time.Duration, maxExp int64) expBackoff {
+	return expBackoff{baseDelay: baseDelay, maxExp: maxExp}
+}
+
+// getPacketUUID scans the tracking slice for matches.
+func (p *Pool) getPacketUUID(pkt []byte) (packetUUID uuid.UUID, err error) {
+	if err = packetUUID.UnmarshalBinary(pkt[timeSliceLength : timeSliceLength+trackerLength]); err != nil {
+		return
+	}
+	return
+}
+
+// getCurrentTrackerUUID grabs the latest tracker UUID.
+func (p *Ping) getCurrentTrackerUUID() uuid.UUID {
+	return p.uuid
+}
+
+func bytesToTime(b []byte) time.Time {
+	var nSec int64
+	for i := uint8(0); i < 8; i++ {
+		nSec += int64(b[i]) << ((7 - i) * 8)
+	}
+	return time.Unix(nSec/1000000000, nSec%1000000000)
+}
+
+func isIPv4(ip net.IP) bool {
+	return len(ip.To4()) == net.IPv4len
+}
+
+func timeToBytes(t time.Time) []byte {
+	nSec := t.UnixNano()
+	b := make([]byte, 8)
+	for i := uint8(0); i < 8; i++ {
+		b[i] = byte((nSec >> ((7 - i) * 8)) & 0xff)
+	}
+	return b
+}
+
+var seed = time.Now().UnixNano()
+
+// getSeed returns a goroutine-safe unique seed
+func getSeed() int64 {
+	return atomic.AddInt64(&seed, 1)
+}
+
+// stripIPv4Header strips IPv4 header bytes if present
+func stripIPv4Header(n int, b []byte) int {
+	if len(b) < 20 {
+		return n
+	}
+	l := int(b[0]&0x0f) << 2
+	if 20 > l || l > len(b) {
+		return n
+	}
+	if b[0]>>4 != 4 {
+		return n
+	}
+	copy(b, b[l:])
+	return n - l
+}
