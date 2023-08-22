ba26e4e6ada4bde66896b612c268fe80aeb2d82a
code <43371021+zqlpaopao@users.noreply.github.com>
2023-08-22 14:35:33 +0800

    Update pool.go

--- data-any-pool/pkg/pool.go
+++ data-any-pool/pkg/pool.go
@@ -4,13 +4,13 @@ package pkg
 // Data is the type of cached data
 // New that needs to be created when the cache does not exist
 type Pool[T any] struct {
-	Data chan *T
-	New  func(size int) *T
+	Data chan T
+	New  func(size int) T
 	Size int
 }
 
 // Get  the data
-func (p *Pool[T]) Get() (res *T) {
+func (p *Pool[T]) Get() (res T) {
 	select {
 	case res, _ = <-p.Data:
 		return
@@ -20,9 +20,16 @@ func (p *Pool[T]) Get() (res *T) {
 }
 
 // Put the data to cached
-func (p *Pool[T]) Put(res *T) {
+func (p *Pool[T]) Put(res T) {
 	select {
 	case p.Data <- res:
 	default:
 	}
+}
+
+// NewPool make new data pool
+func NewPool[T any](dataSize,
+	poolSize int,
+	f func(size int) T) Pool[T] {
+	return Pool[T]{Data: make(chan T, poolSize), Size: dataSize, New: f}
 }
