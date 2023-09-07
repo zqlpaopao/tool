b40c68f12cd7eb7f46ce7e8c0d1bf744580265e6
code <43371021+zqlpaopao@users.noreply.github.com>
2023-09-07 19:36:50 +0800

    Add files via upload

---
+++ znet/ping/packetconn.go
@@ -0,0 +1,152 @@
+package ping
+
+import (
+	"fmt"
+	"net"
+	"runtime"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
+type packetConn interface {
+	Close() error
+	ICMPRequestType() icmp.Type
+	ReadFrom(b []byte) (n int, ttl int, src net.Addr, err error)
+	SetFlagTTL() error
+	SetReadDeadline(t time.Time) error
+	WriteTo(b []byte, dst net.Addr) (int, error)
+	SetTTL(ttl int)
+	SetMark(m uint) error
+	SetDoNotFragment() error
+}
+
+type icmpConn struct {
+	c   *icmp.PacketConn
+	ttl int
+}
+
+func (c *icmpConn) Close() error {
+	return c.c.Close()
+}
+
+func (c *icmpConn) SetTTL(ttl int) {
+	c.ttl = ttl
+}
+
+func (c *icmpConn) SetReadDeadline(t time.Time) error {
+	return c.c.SetReadDeadline(t)
+}
+
+func (c *icmpConn) WriteTo(b []byte, dst net.Addr) (int, error) {
+	//if c.c.IPv6PacketConn() != nil {
+	//	if err := c.c.IPv6PacketConn().SetHopLimit(c.ttl); err != nil {
+	//		return 0, err
+	//	}
+	//}
+	//if c.c.IPv4PacketConn() != nil {
+	//	if err := c.c.IPv4PacketConn().SetTTL(c.ttl); err != nil {
+	//		return 0, err
+	//	}
+	//}
+
+	return c.c.WriteTo(b, dst)
+}
+
+type icmpV4Conn struct {
+	icmpConn
+}
+
+func (c *icmpV4Conn) SetFlagTTL() error {
+	err := c.c.IPv4PacketConn().SetControlMessage(ipv4.FlagTTL, true)
+	if runtime.GOOS == "windows" {
+		return nil
+	}
+	return err
+}
+
+func (c *icmpV4Conn) ReadFrom(b []byte) (int, int, net.Addr, error) {
+	ttl := -1
+	n, cm, src, err := c.c.IPv4PacketConn().ReadFrom(b)
+	if cm != nil {
+		ttl = cm.TTL
+	}
+	return n, ttl, src, err
+}
+
+func (c icmpV4Conn) ICMPRequestType() icmp.Type {
+	return ipv4.ICMPTypeEcho
+}
+
+type icmpV6Conn struct {
+	icmpConn
+}
+
+func (c *icmpV6Conn) SetFlagTTL() error {
+	err := c.c.IPv6PacketConn().SetControlMessage(ipv6.FlagHopLimit, true)
+	if runtime.GOOS == "windows" {
+		return nil
+	}
+	return err
+}
+
+func (c *icmpV6Conn) ReadFrom(b []byte) (int, int, net.Addr, error) {
+	ttl := -1
+	n, cm, src, err := c.c.IPv6PacketConn().ReadFrom(b)
+	if cm != nil {
+		ttl = cm.HopLimit
+	}
+	return n, ttl, src, err
+}
+
+func (c icmpV6Conn) ICMPRequestType() icmp.Type {
+	return ipv6.ICMPTypeEchoRequest
+}
+
+// makeSendConn4-- --------------------------
+// --> @Describe make the conn4 client
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) makeSendConn4() (packetConn, error) {
+	var (
+		c4  icmpV4Conn
+		err error
+	)
+
+	if c4.c, err = icmp.ListenPacket(ipv4Proto[p.option.protocol], p.option.source); nil != err {
+		return nil, fmt.Errorf("makeConn4--->ListenPacket->protocol-%s ,source-%s,err %v", ipv4Proto[p.option.protocol], p.option.source, err)
+	}
+	//
+	if err = c4.SetFlagTTL(); nil != err {
+		return nil, fmt.Errorf("makeConn4--->SetFlagTTL->protocol-%s ,source-%s,err %v", ipv4Proto[p.option.protocol], p.option.source, err)
+	}
+	c4.SetTTL(p.option.ttl)
+
+	return &c4, nil
+}
+
+// makeConn6 -- --------------------------
+// --> @Describe  make the conn6 client
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) makeSendConn6() (packetConn, error) {
+	var (
+		c6  icmpV6Conn
+		err error
+	)
+	fmt.Println("start v6")
+
+	if c6.c, err = icmp.ListenPacket(ipv6Proto[p.option.protocol], p.option.source); nil != err {
+		return nil, fmt.Errorf("makeConn6--->ListenPacket->protocol-%s ,source-%s,err %v", ipv6Proto[p.option.protocol], p.option.source, err)
+	}
+	if err = c6.SetFlagTTL(); nil != err {
+		return nil, fmt.Errorf("makeConn6--->SetFlagTTL->protocol-%s ,source-%s,err %v", ipv6Proto[p.option.protocol], p.option.source, err)
+	}
+	c6.SetTTL(p.option.ttl)
+
+	return &c6, nil
+}
---
+++ znet/ping/receive.go
@@ -0,0 +1,354 @@
+package ping
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"runtime"
+	"time"
+)
+
+// revIpv4 -- --------------------------
+// --> @Describe revIpv4 rev the ipv4 packet
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) revIpv4(conn net.PacketConn) {
+	defer p.option.Recover()
+	// Start by waiting for 50 µs and increase to a possible maximum of ~ 100 ms.
+	expBackoffTime := newExpBackoff(50*time.Microsecond, 11)
+	delay := expBackoffTime.Get()
+	var (
+		bytes []byte
+		n     int
+		err   error
+		addr  net.Addr
+	)
+	if p.option.protocol != "icmp" && runtime.GOOS == "darwin" {
+		bytes = make([]byte, p.getMessageLength()+NoIcmpAndDarwin)
+	} else {
+		bytes = make([]byte, p.getMessageLength())
+	}
+	for {
+		select {
+		case <-p.done:
+			goto END
+		default:
+			if err = conn.SetReadDeadline(time.Now().Add(delay)); err != nil {
+				errIn := p.errP.Get()
+				errIn.Tag,
+					errIn.Ping,
+					errIn.Err =
+					"revIpv4-SetReadDeadline",
+					"",
+					errors.New("conn4Rev.SetReadDeadline--->"+err.Error())
+				p.errChan <- errIn
+				continue
+			}
+
+			if n, addr, err = conn.ReadFrom(bytes); err != nil {
+				if netErr, ok := err.(*net.OpError); ok {
+					if netErr.Timeout() {
+						// Read timeout
+						delay = expBackoffTime.Get()
+						continue
+					}
+				}
+				errIn := p.errP.Get()
+				errIn.Tag,
+					errIn.Ping,
+					errIn.Err =
+					"revIpv4-ReadFrom",
+					"",
+					errors.New("conn4Rev.SetReadDeadline--->"+err.Error())
+				p.errChan <- errIn
+				continue
+			}
+
+			//packet := gopacket.NewPacket(bytes, layers.LayerTypeICMPv4, gopacket.NoCopy)
+			//packet = packet
+			//fmt.Println("revIp4", id, packet)
+			//os.Exit(1)
+			//	// Get the UDP layer from this packet
+			//	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+			//		udp, _ := udpLayer.(*layers.UDP)
+			//		if app := packet.ApplicationLayer(); app != nil {
+			//			data, err := EncodeUDPPacket(net.ParseIP("127.0.0.1"), net.ParseIP("127.0.0.1"), uint16(udp.DstPort), uint16(udp.SrcPort), app.Payload())
+			//			if err != nil {
+			//				log.Printf("failed to EncodePacket: %v", err)
+			//				return
+			//			}
+			//			if _, err := conn.WriteTo(data, remoteaddr); err != nil {
+			//				log.Printf("failed to write packet: %v", err)
+			//				conn.Close()
+			//				return
+			//			}
+			//		}
+			//	}
+			//}
+
+			if err = p.processPacket(time.Now(),
+				addr,
+				bytes,
+				n,
+				true); nil != err {
+				errIn := p.errP.Get()
+				errIn.Tag,
+					errIn.Ping,
+					errIn.Err =
+					"revIpv4-ReadFrom",
+					"",
+					errors.New("conn4Rev.processPacket--->"+err.Error())
+				p.errChan <- errIn
+
+				continue
+			}
+		}
+	}
+END:
+	_ = conn.Close()
+	p.wgRec.Done()
+}
+
+// revIpv6 -- --------------------------
+// --> @Describe revIpv6 rev the ipv6 packet
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) revIpv6(conn net.PacketConn) {
+	defer p.option.Recover()
+	// Start by waiting for 50 µs and increase to a possible maximum of ~ 100 ms.
+	expBackoffTime := newExpBackoff(50*time.Microsecond, 11)
+	delay := expBackoffTime.Get()
+
+	var (
+		bytes []byte
+		n     int
+		err   error
+		addr  net.Addr
+	)
+	if p.option.protocol != "icmp" && runtime.GOOS == "darwin" {
+		bytes = make([]byte, p.getMessageLength()+NoIcmpAndDarwin)
+	} else {
+		bytes = make([]byte, p.getMessageLength())
+	}
+
+	for {
+		select {
+		case <-p.done:
+			goto END
+		default:
+			if err = conn.SetReadDeadline(time.Now().Add(delay)); err != nil {
+
+				errIn := p.errP.Get()
+				errIn.Tag,
+					errIn.Ping,
+					errIn.Err =
+					"revIpv4-ReadFrom",
+					"",
+					errors.New("conn6Rev.SetReadDeadline--->"+err.Error())
+				p.errChan <- errIn
+
+				continue
+			}
+			if n, addr, err = conn.ReadFrom(bytes); err != nil {
+				if netErr, ok := err.(*net.OpError); ok {
+					if netErr.Timeout() {
+						// Read timeout
+						delay = expBackoffTime.Get()
+						continue
+					}
+				}
+
+				errIn := p.errP.Get()
+				errIn.Tag,
+					errIn.Ping,
+					errIn.Err =
+					"revIpv4-ReadFrom",
+					"",
+					errors.New("conn6Rev.ReadFrom--->"+err.Error())
+				p.errChan <- errIn
+
+				continue
+			}
+			if err = p.processPacket(time.Now(),
+				addr,
+				bytes,
+				n,
+				false); nil != err {
+
+				errIn := p.errP.Get()
+				errIn.Tag,
+					errIn.Ping,
+					errIn.Err =
+					"revIpv6-ReadFrom",
+					"",
+					errors.New("conn6Rev.processPacket--->"+err.Error())
+				p.errChan <- errIn
+
+				continue
+			}
+		}
+	}
+END:
+	_ = conn.Close()
+	p.wgRec.Done()
+}
+
+// matchID-- --------------------------
+// --> @Describe  Attempts to match the ID of an ICMP packet.
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) matchID(ID int, id int) bool {
+	// On Linux we can only match ID if we are privileged.
+	if p.option.protocol == "icmp" {
+		return ID == id
+	}
+	return true
+}
+
+// getMessageLength -- --------------------------
+// --> @Describe Returns the length of an ICMP message.
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) getMessageLength() int {
+	return p.option.dataSize + 8
+}
+
+// processPacket -- --------------------------
+// --> @Describe process the packet
+// --> @params
+// --> @return
+// -- ------------------------------------
+func (p *Pool) processPacket(receivedAt time.Time,
+	addr net.Addr,
+	bytes []byte,
+	nBytes int,
+	ipV4 bool) (err error) {
+	var (
+		proto   int
+		ip      string
+		pktUUID string
+		isEcho  bool
+		Id      int
+		seq     int
+		data    []byte
+	)
+	if ipV4 {
+		proto = protocolICMP
+		nBytes = stripIPv4Header(nBytes, bytes)
+	} else {
+		proto = protocolIPv6ICMP
+	}
+	//if m, err = icmp.ParseMessage(proto, bytes); err != nil {
+	//	//if m, err = icmp.ParseMessage(proto, bytes); err != nil {
+	//	return fmt.Errorf("error parsing icmp message: %w", err)
+	//}
+
+	if isEcho, Id, seq, data, err = ParseMessage(proto, bytes); err != nil {
+		return fmt.Errorf("error parsing icmp message: %w", err)
+	}
+
+	//if m.Type != ipv4.ICMPTypeEchoReply && m.Type != ipv6.ICMPTypeEchoReply {
+	//	// Not an echo reply, ignore it
+	//	return nil
+	//}
+
+	if !isEcho {
+		// Not an echo reply, ignore it
+		return nil
+	}
+
+	if !p.matchID(Id, p.option.pid) {
+		return nil
+	}
+
+	if p.option.protocol == Udp {
+		if ip, _, err = net.SplitHostPort(addr.String()); err != nil {
+			return fmt.Errorf("err ip : %v, err %v", addr, err)
+		}
+	} else {
+		ip = addr.String()
+	}
+	if len(data) < timeSliceLength+trackerLength {
+		return fmt.Errorf("insufficient data received; got: %d %v",
+			len(data), data)
+	}
+	if pktUUID = p.getPacketUUID(data); err != nil {
+		return err
+	}
+
+	if _, ok := p.get(pktUUID); !ok {
+		return errors.New("not have the uuid")
+	}
+	pkg := p.PacketP.Get()
+	pkg.StartTime,
+		pkg.EndTime,
+		pkg.Addr,
+		pkg.NBytes,
+		pkg.Seq,
+		pkg.ID,
+		pkg.uuid =
+		bytesToTime(data[:timeSliceLength]),
+		receivedAt,
+		ip,
+		nBytes,
+		seq,
+		Id,
+		pktUUID
+	p.resChan <- pkg
+
+	//switch pkt := m.Body.(type) {
+	//case *icmp.Echo:
+	//	if !p.matchID(pkt.ID, p.option.pid) {
+	//		return nil
+	//	}
+	//
+	//	if p.option.protocol == Udp {
+	//		if ip, _, err = net.SplitHostPort(addr.String()); err != nil {
+	//			return fmt.Errorf("err ip : %v, err %v", addr, err)
+	//		}
+	//	} else {
+	//		ip = addr.String()
+	//	}
+	//
+	//	if len(pkt.Data) < timeSliceLength+trackerLength {
+	//		return fmt.Errorf("insufficient data received; got: %d %v",
+	//			len(pkt.Data), pkt.Data)
+	//	}
+	//	if pktUUID = p.getPacketUUID(pkt.Data); err != nil {
+	//		return err
+	//	}
+	//
+	//	if _, ok := p.get(pktUUID); !ok {
+	//		return errors.New("not have the uuid")
+	//	}
+	//	pkg := p.PacketP.Get()
+	//	pkg.StartTime,
+	//		pkg.EndTime,
+	//		pkg.Addr,
+	//		pkg.NBytes,
+	//		pkg.Seq,
+	//		pkg.ID,
+	//		pkg.TTL,
+	//		pkg.uuid =
+	//		bytesToTime(pkt.Data[:timeSliceLength]),
+	//		receivedAt,
+	//		ip,
+	//		nBytes,
+	//		pkt.Seq,
+	//		pkt.ID,
+	//		ttl,
+	//		pktUUID
+	//
+	//	p.resChan <- pkg
+	//
+	//default:
+	//	// Very bad, not sure how this can happen
+	//	return fmt.Errorf("invalid ICMP echo reply; type: '%T', '%v'", pkt, pkt)
+	//}
+
+	return
+}
